b44544777188eb398f83c798e5698f593ad6e1f3
Adam <adamelmadani0129@gmail.com>
2023-08-30 21:33:12 +0000

    Fix 0-task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -35,13 +35,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	if (parent->left == NULL)
-	{
-		parent->left = node;
-	}
-	else
-	{
-		parent->right = node;
-	}
 	return (node);
 }
